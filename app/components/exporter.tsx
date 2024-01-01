1107613b7f34d35d20ed87663da0427aa7c9e349
lichengzhidegithub <154681628+lichengzhidegithub@users.noreply.github.com>
2024-01-01 17:21:22 +0800

    Update exporter.tsx

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -558,14 +558,7 @@ export function ImagePreviewer(props: {
             <div className={styles["chat-info-item"]}>
               {Locale.Exporter.Topic}: {session.topic}
             </div>
-           {/*
-<div className={styles["chat-info-item"]}>
-  {Locale.Exporter.Time}:{" "}
-  {new Date(
-    props.messages.at(-1)?.date ?? Date.now(),
-  ).toLocaleString()}
-</div>
-*/}
+      
           </div>
         </div>
         {props.messages.map((m, i) => {
